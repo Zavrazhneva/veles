27345972b5cc9d5217b26dbe4e70a3eaab6d901e
Anastasiya <zavrazhneva.a@gmail.com>
2020-06-19 00:11:00 +0300

    add pagination jsx

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import {Footer} from '../header/Header';
 import {Offer} from '../offer/Offer';
 import S from './App.module.css'
-import {Content} from '../content/Content'
+import {Content} from '../content/Content';
+import {Pagination} from '../pagination/Pagination';
 
 
 export class App extends React.Component {
@@ -13,6 +14,12 @@ export class App extends React.Component {
                 <h2 className={S.title}>Новости культуры</h2>
                 <Footer/>
                 <Offer/>
+                <Pagination  pagesCount={30} activePage={3} onPageChange={(i)=>{
+                  console.log(i)
+                }}/>
+                 <Pagination  pagesCount={3} activePage={3} onPageChange={(i)=>{
+                  console.log(i)
+                }}/>
                 <Content/>
             </div>
         )
